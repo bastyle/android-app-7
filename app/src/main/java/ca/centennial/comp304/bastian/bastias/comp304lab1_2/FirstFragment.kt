9e2002375a235a76d7fd8656c8b2f5e2b74eedd5
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2023-09-21 19:43:15 -0400

    added strings list

--- app/src/main/java/ca/centennial/comp304/bastian/bastias/comp304lab1_2/FirstFragment.kt
+++ app/src/main/java/ca/centennial/comp304/bastian/bastias/comp304lab1_2/FirstFragment.kt
@@ -1,7 +1,57 @@
 package ca.centennial.comp304.bastian.bastias.comp304lab1_2
 
+import android.R
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import ca.centennial.comp304.bastian.bastias.comp304lab1_2.databinding.FragmentFirstBinding
 
-class FirstFragment:Fragment() {
+class FirstFragment:Fragment(ca.centennial.comp304.bastian.bastias.comp304lab1_2.R.layout.fragment_first) {
+    companion object{
+        private const val TAG = "FirstFragment"
+    }
 
+    var mobileArray = arrayOf(
+        "Android", "IPhone", "WindowsMobile", "Blackberry",
+        "WebOS", "Ubuntu", "Windows7", "Max OS X"
+    )
+
+    private lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = ca.centennial.comp304.bastian.bastias.comp304lab1_2.databinding.FragmentFirstBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG, "onStart")
+        // Sample data
+        val items = arrayOf<String>("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
+        val items2 = resources.getStringArray(ca.centennial.comp304.bastian.bastias.comp304lab1_2.R.array.activities)
+        //Toast.makeText(requireContext(),getString(ca.centennial.comp304.bastian.bastias.comp304lab1_2.R.string.app_name),Toast.LENGTH_LONG).show()
+        val adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_single_choice, items2)
+
+        binding.listView.adapter=adapter
+        binding.listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE)
+        binding.listView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, position, id ->
+            val selectedItem = adapterView.getItemAtPosition(position) as String
+            val itemIdAtPos = adapterView.getItemIdAtPosition(position)
+
+
+            Toast.makeText(requireContext(),"click item $selectedItem its position $itemIdAtPos",Toast.LENGTH_SHORT).show()
+        }
+
+    }
 }
--- app/src/main/java/ca/centennial/comp304/bastian/bastias/comp304lab1_2/SecondFragment.kt
+++ app/src/main/java/ca/centennial/comp304/bastian/bastias/comp304lab1_2/SecondFragment.kt
@@ -2,6 +2,6 @@ package ca.centennial.comp304.bastian.bastias.comp304lab1_2
 
 import androidx.fragment.app.Fragment
 
-class SecondFragment:Fragment() {
+class SecondFragment:Fragment(R.layout.fragment_second) {
 
 }

a8015ce2cbc81b6482fa7c265506f33176636716
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2023-09-21 17:07:56 -0400

    added fragments

---
+++ app/src/main/java/ca/centennial/comp304/bastian/bastias/comp304lab1_2/FirstFragment.kt
@@ -0,0 +1,7 @@
+package ca.centennial.comp304.bastian.bastias.comp304lab1_2
+
+import androidx.fragment.app.Fragment
+
+class FirstFragment:Fragment() {
+
+}
--- app/src/main/java/ca/centennial/comp304/bastian/bastias/comp304lab1_2/MainActivity.kt
+++ app/src/main/java/ca/centennial/comp304/bastian/bastias/comp304lab1_2/MainActivity.kt
@@ -2,10 +2,32 @@ package ca.centennial.comp304.bastian.bastias.comp304lab1_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import ca.centennial.comp304.bastian.bastias.comp304lab1_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firstFragment: FirstFragment
+
+    companion object {
+        private const val TAG = "MainActivity Lab 1 Exercise 2"
+        public const val MSG_KEY="MSG"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.e(TAG,"onCreate")
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ca.centennial.comp304.bastian.bastias.comp304lab1_2.databinding.ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firstFragment= FirstFragment()
+        val secondFragment= SecondFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+
+        fragmentTransaction.add(R.id.fragment_container_1, firstFragment, "FirstFragment")
+        fragmentTransaction.add(R.id.fragment_container_2, secondFragment, "SecondFragment")
+        fragmentTransaction.commit()
+        Toast.makeText(this,"Toast from Activity", Toast.LENGTH_LONG).show()
     }
 }
